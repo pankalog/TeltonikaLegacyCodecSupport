36f22c4092997e13250d5a25f1bb057380a42a0c
Panos Kalogeropoulos <pan.kalog16@gmail.com>
2023-11-10 16:11:25 +0100

    Improved parser responses

--- src/listenForDevice.ts
+++ src/listenForDevice.ts
@@ -1,32 +1,20 @@
-import { Socket } from 'net';
 import { ProtocolParser, parseIMEI, Data, GPRS } from 'complete-teltonika-parser';
 
 export function listenForDevice(response: Buffer) : {'Content': ProtocolParser | undefined, 'Imei': string} {
-  var imei = undefined;
-  const buf = Buffer.from(response);
-  // Extract the source and destination IP addresses from the buffer
-  
-  const packet = response.toString("hex");
-  
-  if (packet.length === 34) {
-    imei = parseIMEI(packet);
-
-    return {'Content': undefined, 'Imei': imei}; 
-  }
-  else {
-    let parsed = new ProtocolParser(packet);
-    const dataLength = parsed.Data_Length;
-    // console.log("CodecType:", parsed.CodecType);
-    
-    if (parsed.CodecType == "data sending") {
-      // console.log(parsed);
-      
-      return {'Content': parsed, 'Imei': imei};
-      
-    }
-  }
+    var imei = undefined;
+    const packet = response.toString("hex");
+    var processedPacket = processPacket(packet)
+    return {'Content': processedPacket.dataPacket, 'Imei': processedPacket.imei};
 }
 
-
-
-
+function processPacket (packet) {
+    if (packet.length == 34) {
+        return {
+            imei: parseIMEI(packet)
+        }
+    } else {
+        return { 
+            dataPacket: new ProtocolParser(packet)
+        }
+    }
+}
